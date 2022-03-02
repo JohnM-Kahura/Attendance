24eba51ac717d9fd515f73b997477ff78b94075c
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-02 19:33:23 +0300

    widget binding

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:attendance/auth/register.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
