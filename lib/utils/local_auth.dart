b5ee3731961b17da1183bc0346101a71e5ac9c5f
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-02 19:33:08 +0300

    fingerprint initialization

---
+++ lib/utils/local_auth.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+class LocalAuthApi {
+  static final _auth = LocalAuthentication();
+
+
+  static Future<bool> hasBiometrics() async{
+    return _auth.canCheckBiometrics;
+  }
+  static Future<bool> authenticate() async {
+    final bool  isBiometrics=await hasBiometrics();
+    if(!isBiometrics) return false;
+   try {
+      return await _auth.authenticate(
+      localizedReason: 'Scan Fingerprint to Authenticate',
+      biometricOnly: true,
+      useErrorDialogs: true,
+      stickyAuth: true,
+    );
+   } on PlatformException catch (e) {
+   print(e);
+   return false;
+   }
+  }
+}
