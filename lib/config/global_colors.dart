9374238f6b931e3c7434022894070f3a6aca58a9
JohnM-Kahura <Johnmungai392@gmail.com>
2022-03-02 10:35:50 +0300

    global colors

---
+++ lib/config/global_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Color authThemeColor = const Color(0xFFE6EAED);
+Color buttonColor = const Color(0xFFFCDD6D);
+Color textColorBlack = const Color(0xFF000000);
+Color customBlue = const Color(0xFF7AC9F0);
+Color customBlack = const Color(0xFF4E5254);
